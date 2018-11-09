fb90d5ae3209439ab342865dd61d83308f89e949
Axyom <32022099+Axyom@users.noreply.github.com>
2018-11-09 16:59:12 -0500

    c'est l exo 2 ca

--- exo3.pl
+++ exo3.pl
@@ -1,48 +1,146 @@
-%% BASE DE CONNAISSANCE
+% Student exercise profile
+:- set_prolog_flag(occurs_check, error).        % disallow cyclic terms
+:- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
+:- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
+:- discontiguous anglais/1.
+:- discontiguous politicien/1.
+% Your program goes here
+/*ask(vivant) :-
+  format('Est-il vivant ? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(artiste) :-
+  format('Est ce un artiste? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(fictif) :-
+  format('Est il fictif? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(anglais) :-
+  format('Est il anglophone? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(jeux) :-
+  format('Est ce un personnage de jeux-video? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(chant) :-
+  format('Est ce un/e chanteur/chanteuse? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(cinema) :-
+  format('Est-il dans le monde du cinema? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(ecrire) :-
+  format('Est ce un/e auteur? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(homme) :-
+  format('Est ce un hommme? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(femme) :-
+  format('Est ce une femme? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(politicien) :-
+  format('Est ce un/une politician/nne? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(realisateur) :-
+  format('Est ce un/une realisateur/trice? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(sport) :-
+  format('Fais-il du sport? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(religieux) :-
+  format('Est ce un religieux? '),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(art, X) :-
+  format('Est ce un artiste de ~w ? ', [X]),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(gouverne, Y) :-
+  format('Gouverne-t il le ~w  ? ', [Y]),
+  read(Reponse),
+  Reponse = 'oui'.
+ask(religion, Y) :-
+  format('Est-il ~w  ? ', [Y]),
+  read(Reponse),
+  Reponse = 'oui'.
+*/
 
-cours(inf1600).
-cours(inf1900).
-cours(inf2205).
-cours(log1000).
-cours(inf1005C).
-cours(inf1500).
-cours(inf1010).
-cours(inf2010).
-cours(inf2705).
-cours(log2410).
-cours(log2810).
-cours(log2990).
-cours(mth1007).
+ask(X) :-
+  format('A t il cet attribut: ~w  ? ', [X]),
+  read(Reponse),
+  Reponse = 'oui'.
 
-prerequis(inf1010,inf1005C).
-prerequis(log1000,inf1005C).
-prerequis(inf2705,inf2010).
-prerequis(inf2010,inf1010).
-prerequis(inf1600,inf1005C).
-prerequis(inf1600,inf1500).
-prerequis(log2410,log1000).
-prerequis(log2410,inf1010).
+ask(X,Y) :-
+  format('~w de ~w ? ', [X,Y]),
+  read(Reponse),
+  Reponse = 'oui'.
 
-corequis(inf2010,log2810).
-corequis(inf2705,log2990).
-corequis(inf2705,mth1007).
-corequis(inf1900,log1000).
-corequis(inf1900,inf1600).
-corequis(inf1900,inf2205).
+personne(X) :- ask(vivant), vivant(X).
+personne(X) :- ask(politicien), politicien(X).
+personne(X) :- ask(religieux), religieux(X).
+personne(X) :-  ask(chant),chanteur(X).
+personne(X) :- sportif(X), mort(X).
 
-coursPrerequis(A, B) :- prerequis(A, B).
-coursPrerequis(A, B) :-
-					prerequis(A, X),
-					coursPrerequis(X, B).
+vivant(X) :-  (ask(artiste),artiste(X));(ask(fictif),fictif(X));(ask(religieux),chretien(X);(ask(russe),russe(X));(ask(sportif),(sportif(X),vit(X))));createur(X).
+fictif(X) :- (ask(anglais), anglais(X));jeux(X).
+anglais(X) :- ask(jeux),jeuxA(X).
 
-coursCorequis(A, B) :- corequis(A, B).
-coursCorequis(A, B) :-
-					corequis(A, X),
-					coursCorequis(X, B).
+artiste(X) :- (   ask(homme), artisteH(X));artisteF(X).
 
-coursRequis(A, B) :- (corequis(A, B) ; prerequis(A, B)).
-coursRequis(A, B) :-
-					(corequis(A, X) ; prerequis(A, X)),
-					coursRequis(X, B).
+artisteF(X) :-  ask(cinema), actrice(X).
+artisteF(X) :-  (ask(litterature), auteure(X));chanteuse(X).
 
-courAPrendreComplet(C, L) :- setof(X, coursRequis(C, X), L).
+artisteH(X) :-  (ask(cinema), cinema(X));( peintre(X) ).
+cinema(X) :- (ask(realisateur), realisateur(X));acteur(X).
+
+politicien(X):- ask(anglais),((ask(raciste), raciste(X));(non_raciste(X))).
+politicien(X) :- (ask(russe),gouverne(X,urss));((ask(homme),auteur(X));gouverne(X,egypt)).
+religieux(X) :-  prophete(X,Y), religion(Y), ask(religion,Y).
+
+
+sportif(fernando_alonso).
+vit(fernando_alonso).
+sportif(ayrton_senna).
+mort(ayrton_senna).
+peintre(bansky).
+createur(hideo_Kojima).
+chretien(pape_francois).
+prophete(moise,juif).
+prophete(jesus,chretien).
+anglais(james_bond).
+jeux(mario).
+jeuxA(lara_croft).
+chanteuse(celine_dion).
+chanteur(michael_Jackson).
+auteure(j_k_Rowling).
+auteur(victor_Hugo).
+raciste(richard_nixon).
+non_raciste(dwighlight_D_eisenhower).
+actrice(jennifer_lawence).
+realisateur(quentin_Tarantino).
+acteur(denzel_Washinton).
+russe(mikhail_Gorbachev).
+/*gouverne(mikhail_Gorbachev,urss).*/
+gouverne(joseph_staline,urss).
+gouverne(dwighlight_D_eisenhower,usa).
+gouverne(richard_nixon,usa).
+gouverne(cleopatre,egypt).
+pays(urss).
+pays(usa).
+pays(egypt).
+religion(juif).
+religion(chretien).
+
+/** <examples> Your example queries go here, e.g.
+?- member(X, [cat, mouse]).
+*/
